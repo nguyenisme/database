8b5232acc05e93f406d796a792002fcf88a06586
KHAC NGUYEN <KHAC NGUYEN@DESKTOP-555IDLF>
2025-05-10 10:29:22 +0700

    Add project files.

---
+++ database/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace database
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button_sach = new Button();
+            SuspendLayout();
+            // 
+            // button_sach
+            // 
+            button_sach.Location = new Point(95, 133);
+            button_sach.Name = "button_sach";
+            button_sach.Size = new Size(94, 29);
+            button_sach.TabIndex = 0;
+            button_sach.Text = "Sách";
+            button_sach.UseVisualStyleBackColor = true;
+            button_sach.Click += button_sach_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button_sach);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btn_Sach;
+        private Button button_sach;
+    }
+}
